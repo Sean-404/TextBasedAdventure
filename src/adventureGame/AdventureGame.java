76b2abe34b17966e3d8f59f191be615285805479
Sean <seanmckenna404@gmail.com>
2021-09-21 21:46:45 +0100

    added player class with attributes

--- src/adventureGame/AdventureGame.java
+++ src/adventureGame/AdventureGame.java
@@ -1,10 +1,17 @@
 package adventureGame;
 
+import java.util.Scanner;
+
 public class AdventureGame {
+	
+	private static final Scanner s = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to this text based adventure game!" +
+		"\nPlease enter your player name: ");
+		String name = s.nextLine();
+		Player player = new Player(name, 10);
+		
 	}
 
 }
--- src/adventureGame/Player.java
+++ src/adventureGame/Player.java
@@ -1,5 +1,21 @@
 package adventureGame;
 
 public class Player {
+	
+	private String playerName;
+	private int playerHealth;
+	
+	public Player(String playerName, int playerHealth) {
+		this.playerName = playerName;
+		this.playerHealth = playerHealth;
+	}
+	
+	public String getName() {
+		return playerName;
+	}
+	
+	public int getHealth() {
+		return playerHealth;
+	}
 
 }
